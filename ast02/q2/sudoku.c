d9574c9911b8711dbbb4be1492a636c53f10149e
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-10-23 21:38:33 -0300

    docs(ast02 sudoku.c): add comments to describe what is happening

--- ast02/q2/sudoku.c
+++ ast02/q2/sudoku.c
@@ -27,6 +27,7 @@
 
 #define MAX_BUF_LEN 1024
 
+// sudoku structure
 typedef struct sudoku {
    int      size; // equal to alphabet length
    int      cell_size; // width of cell, square root of size
@@ -35,16 +36,20 @@ typedef struct sudoku {
    char *** suppliedPuzzle;
 } Sudoku;
 
+// attribute to thread
 typedef struct {
    int r, c, v;
    Sudoku * s;
 } thread_attr;
+// return from thread
 typedef struct {
    int valid;
 } thread_return;
 
+// shared sudoku
 Sudoku s;
 
+// function returns index of character in the string
 int indexOf(char * str, char * substr) {/*{{{*/
    int i;
    char * pos = strstr(str, substr);
@@ -58,6 +63,7 @@ int indexOf(char * str, char * substr) {/*{{{*/
    return i;
 };/*}}}*/
 
+// thread fn to check if given value in the cell is correct for the row`
 void * rowCheck_thread( void * param ) {/*{{{*/
    Sudoku * s;
    int i, v, r, c, max_r, max_c;
@@ -84,6 +90,7 @@ void * rowCheck_thread( void * param ) {/*{{{*/
    ret->valid = 1;
    return (void *) ret;
 };/*}}}*/
+// thread fn to check if given value in the cell is correct for the column
 void * columnCheck_thread( void * param ) {/*{{{*/
    Sudoku * s;
    int i, v, r, c, max_r, max_c;
@@ -110,6 +117,7 @@ void * columnCheck_thread( void * param ) {/*{{{*/
    ret->valid = 1;
    return (void *) ret;
 };/*}}}*/
+// thread fn to check if given value in the cell is correct for the bigger cell
 void * cellCheck_thread( void * param ) {/*{{{*/
    Sudoku * s;
    int i, j, v, r, c, sRow, sCol, max_cell;
@@ -140,6 +148,8 @@ void * cellCheck_thread( void * param ) {/*{{{*/
    ret->valid = 1;
    return (void *) ret;
 };/*}}}*/
+// fn to check if value is correct in the given cell
+// implemented with thread fns above
 int checkValid(Sudoku * s, int r, int c, int v) {/*{{{*/
    int valid;
    pthread_t row_tid, column_tid, cell_tid;
@@ -149,22 +159,28 @@ int checkValid(Sudoku * s, int r, int c, int v) {/*{{{*/
                  * column_valid = malloc(sizeof(thread_return)),
                  * cell_valid = malloc(sizeof(thread_return));
 
+   // create argument object for each thread
    arg.s = s;
    arg.r = r;
    arg.c = c;
    arg.v = v;
 
+   // prepare argument
    pthread_attr_init(&row_tattr);
    pthread_attr_init(&column_tattr);
    pthread_attr_init(&cell_tattr);
 
+   // start thread that checks if row is correct
    pthread_create(&row_tid, &row_tattr, rowCheck_thread, &arg);
    if (DEBUG) { printf("--- row    tid:%lu:\n", row_tid); }
+   // start thread that checks if column is correct
    pthread_create(&column_tid, &column_tattr, columnCheck_thread, &arg);
    if (DEBUG) { printf("--- column tid:%lu:\n", column_tid); }
+   // start thread that checks if bigger cell is correct
    pthread_create(&cell_tid, &cell_tattr, cellCheck_thread, &arg);
    if (DEBUG) { printf("--- cell   tid:%lu:\n", cell_tid); }
 
+   // wait until all three threads finish
    pthread_join(row_tid, (void *) &row_valid);
    pthread_join(column_tid, (void *) &column_valid);
    pthread_join(cell_tid, (void *) &cell_valid);
@@ -176,6 +192,8 @@ int checkValid(Sudoku * s, int r, int c, int v) {/*{{{*/
    }
 
 
+   // if all three threads return valid for current combination of row, column
+   // and value, then this value is possible for this cell
    if (row_valid->valid && column_valid->valid && cell_valid->valid) {
       valid = 1;
    } else {
@@ -188,8 +206,11 @@ int checkValid(Sudoku * s, int r, int c, int v) {/*{{{*/
 
    return valid;
 }/*}}}*/
+// fn to check if value is correct in the cell without threads
 int checkValid_sync(Sudoku * s, int r, int c, int v) {/*{{{*/
    int i, j, sRow, sCol, maxCellSize;
+
+   // check if row and column are correct
    for (i = 0; i < s->size; i++) {
       if (s->puzzle[i][c] == v || s->puzzle[r][i] == v)
          return 0;
@@ -200,6 +221,7 @@ int checkValid_sync(Sudoku * s, int r, int c, int v) {/*{{{*/
    sRow = (r / maxCellSize) * maxCellSize;
    sCol = (c / maxCellSize) * maxCellSize;
 
+   // check if current bigger cell is correct
    for (i = sRow; i < sRow + maxCellSize; i++) {
       for (j = sCol; j < sCol + maxCellSize; j++) {
          if (s->puzzle[i][j] == v)
@@ -210,13 +232,17 @@ int checkValid_sync(Sudoku * s, int r, int c, int v) {/*{{{*/
    return 1;
 }/*}}}*/
 
+// read one sudoku from file
 Sudoku readSudoku(FILE * f) {/*{{{*/
    int i, j, size, cell_size;
    char * alph  = (char *) malloc(MAX_BUF_LEN * sizeof(char)),
         * buf = (char *) malloc(MAX_BUF_LEN * sizeof(char));
 
+   // first line of the sudoku block is the alphabet
    fscanf(f, "%s", alph);
+   // size of our sudoku
    size = strlen(alph);
+   // size of our bigger cell
    cell_size = (int) sqrt(size);
 
    if (DEBUG) {
@@ -231,7 +257,9 @@ Sudoku readSudoku(FILE * f) {/*{{{*/
       .puzzle = (int **) malloc( size * sizeof(int *) ),
       .suppliedPuzzle = (char ***) malloc( size * sizeof(char **) )
    };
+   // save alphabet
    strcpy(s.alphabet, alph);
+   // allocate memory for suppliedPuzzle and puzzle
    for (i = 0; i < size; i++) {
       s.puzzle[i] = (int *) malloc( size * sizeof(int) );
       s.suppliedPuzzle[i] = (char **) malloc( size * sizeof(char *) );
@@ -240,6 +268,9 @@ Sudoku readSudoku(FILE * f) {/*{{{*/
       }
    }
 
+   // read size*size symbols from the file and save them to suppliedPuzzle
+   // section and convert them to numbers and save to puzzle section according
+   // to the alphabet provided in the beginning
    for (i = 0; i < size; i++) {
       for (j = 0; j < size; j++) {
          fscanf(f, "%s", buf);
@@ -259,10 +290,12 @@ Sudoku readSudoku(FILE * f) {/*{{{*/
 
    return s;
 };/*}}}*/
+// print provided sudoku and solution
 void printSudoku(Sudoku s) {/*{{{*/
    int i, j, k;
    char c;
 
+   // print sudoku that was given
    printf("Given sudoku\n");
    for (i = 0; i < s.size; i++) {
       for (j = 0; j < s.size; j++) {
@@ -271,6 +304,7 @@ void printSudoku(Sudoku s) {/*{{{*/
       printf("\n");
    }
 
+   // print solved sudoku using alphabet provided
    printf("Solved sudoku\n");
    for (i = 0; i < s.size; i++) {
       for (j = 0; j < s.size; j++) {
@@ -282,15 +316,19 @@ void printSudoku(Sudoku s) {/*{{{*/
       printf("\n");
    }
 };/*}}}*/
+// print internal representation of sudoku for debugging
 void debugPrintSudoku(Sudoku s) {/*{{{*/
    int i, j;
    char * buf = malloc(MAX_BUF_LEN * sizeof(char)),
         * d = malloc(MAX_BUF_LEN * sizeof(char)),
         * delim = malloc(MAX_BUF_LEN * sizeof(char));
 
+   // start buffer string
    strcpy(buf, "");
+   // provide delimeter after printed sudoku
    strcpy(delim, "--------------------------\n");
 
+   // build the buffer string to print to console
    for (i = 0; i < s.size; i++) {
       for (j = 0; j < s.size; j++) {
          sprintf(d, "%2d", s.puzzle[i][j]);
@@ -300,78 +338,112 @@ void debugPrintSudoku(Sudoku s) {/*{{{*/
       strcat(buf, "\n");
    }
 
+   // add delimeter
    strcat(buf, delim);
+   // print to console
    printf("%s", buf);
 
+   // free memory
    free(buf);
    free(d);
    free(delim);
 }/*}}}*/
+// recursive fn to solve sudoku, invoked with sudoku and starting cell 0,0
 int solveSudoku(Sudoku * s, int r, int c) {/*{{{*/
    int v, valid;
 
+   // if our current row is more than the size of the sudoku then we went
+   // through all the elements of sudoku and we inserted all correct values
+   // and the sudoku is solved
    if (r == s->size) {
       return 1;
    }
+   // if our current column is more than the size of the sudoku then we went
+   // throught the whole current row and we need to start checking next row;
    if (c == s->size) {
       return solveSudoku(s, r + 1, 0);
    }
 
    if (SOLVER_DEBUG) {
+      // print debug info about current row, column and value in the current
+      // cell
       printf("-- dbg; r:%d:c:%d:P:%d:\n", r, c, s->puzzle[r][c]);
    }
 
+   // if value in current cell is not -1 we skip this cell
+   // it means that if this value have already been provided, we skip the cell
    if (s->puzzle[r][c] != -1) {
       return solveSudoku(s, r, c + 1);
    }
 
    for (v = 0; v < s->size; v++) {
       if (SOLVER_DEBUG) {
+         // print debug info about current row, column, cell value and value
+         // that is being tested. Here value in the cell has always be -1
          printf("-- dbg; r:%d:c:%d:P:%d:v:%d:\n", r, c, s->puzzle[r][c], v);
       }
+      // check if we can put the value v in the current cell. If macros set to
+      // use one thread, then we use function without threads otherwise we use
+      // function with threads.
       if (ST) {
          valid = checkValid_sync(s, r, c, v);
       } else {
          valid = checkValid(s, r, c, v);
       }
+      // if we are able to insert this number to the cell then insert it and go
+      // to the next cell. Otherwise, do not insert and go to next value of v.
       if (valid) {
          s->puzzle[r][c] = v;
          if (PRINT_EACH_STEP) {
             debugPrintSudoku(*s);
          }
 
+         // go to the next cell after we inserted the value to the current cell
          if (solveSudoku(s, r, c + 1)) {
             return 1;
          }
       }
    }
 
+   // we went through all the values that can be written in the current cell
+   // and didnt find any matching one, then we need to return current cell
+   // to the unknown value
    s->puzzle[r][c] = -1;
 
    return 0;
 }/*}}}*/
 
 int main(int argc, char * argv[]) {
+   // filename has to be second argument
    if (argc > 2 || argc <= 1) {
       printf("Usage: %s <filename>\n", argv[0]);
       exit(1);
    }
 
+   // open file for read
    FILE * file = fopen(argv[1], "r");
    if (!file) {
       return 1;
    }
    if (DEBUG) {
+      // print file memory address
       printf("--- file - p:%p:\n", file);
    }
 
+   // while there is stuff in the file
    while (!feof(file)) {
+      // read sudoku
       s = readSudoku(file);
 
+      // if this sudoku is not empty
       if (s.size > 0) {
+         // solve sudoku and return result
+         // 1 - solved
+         // 0 - not solved
          int result = solveSudoku(&s, 0, 0);
          printf("-------------------------------------------------\n");
          if (result) {
+            // print sudoku
             printSudoku(s);
          } else {
             printf("No solution\n");
