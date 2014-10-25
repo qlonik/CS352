f581a6ca6a442733200ad2eede535fc13899d105
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-10-24 21:29:09 -0300

    feat(ast01): question 3 start
    Imported queue structure from provided examples. Added random number
    generation.

---
+++ ast01/src/forks.c
@@ -0,0 +1,87 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+#define nil 0
+
+typedef struct Node {
+   int info;
+   struct Node * link;
+} node;
+
+typedef struct Queue {
+   struct Node * front;
+   struct Node * back;
+} queue;
+
+queue initQueue(queue q) {/*{{{*/
+   q.front = nil;
+   q.back = nil;
+   return q;
+}/*}}}*/
+queue enqueue(queue q, int info) {/*{{{*/
+   node * n = (node *) malloc(sizeof(node));
+
+   n->link = nil;
+   n->info = info;
+
+   if (q.front == nil) {
+      q.front = n;
+   } else {
+      q.back->link = n;
+   }
+   q.back = n;
+
+   return q;
+}/*}}}*/
+queue dequeue(queue q, int * rinfo) {/*{{{*/
+   node * n = q.front;
+
+   if (q.front != nil) {
+      *rinfo = q.front->info;
+      q.front = q.front->link;
+      free(n);
+   }
+
+   return q;
+}/*}}}*/
+
+void debugPrintQueue(queue q) {/*{{{*/
+   node * n = q.front;
+   printf("f -> ");
+   while (n != nil) {
+      printf("%d -> ", n->info);
+      n = n->link;
+   }
+   printf("b\n");
+}/*}}}*/
+
+int main(int argc, char * argv[]) {
+   int n = 0, r, i;
+
+   if (argc == 2) {
+      n = strtol(argv[1], NULL, 10);
+   } else {
+      printf("Wrong input. Input format: %s <number>\n", argv[0]);
+      exit(1);
+   }
+
+   printf("- n: %d\n", n);
+
+   srand(time(NULL));
+   for (i = 0; i < n; ++i) {
+      r = rand() % 10;
+      printf("- r: %d\n", r);
+   }
+
+   queue odd_pid, even_pid;
+   initQueue(odd_pid);
+   initQueue(even_pid);
+
+   for (i = 0; i < n; i++) {
+   }
+
+   return 0;
+}
