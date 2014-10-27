f86eed0d915668e79f449f5483ae8510753e5764
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-10-26 23:45:04 -0300

    feat(ast01 forks): finish program
    This program takes number n as parameter and generates n child
    processes which executes for random amount of time (not more than 10 sec).
    
    Here processes with odd pids are saved in one queue, and with even pids
    are saved into other queue. Then they are run one after another.

--- ast01/src/forks.c
+++ ast01/src/forks.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 
 #define nil 0
 
@@ -54,7 +62,8 @@ void debugPrintQueue(queue q) {/*{{{*/
 }/*}}}*/
 
 int main(int argc, char * argv[]) {
-   int n = 0, r, i;
+   int n = 0, i, currPid, nextToResume = 0, totalOdd = 0, totalEven = 0,
+       * pid, * wait;
 
    if (argc == 2) {
       n = strtol(argv[1], NULL, 10);
@@ -63,12 +72,19 @@ int main(int argc, char * argv[]) {
       exit(1);
    }
 
-   printf("- n: %d\n", n);
+   if (DEBUG) {
+      printf("- n: %d\n", n);
+   }
+
+   wait = (int *) calloc(n, sizeof(int));
+   pid = (int *) calloc(n, sizeof(int));
 
    srand(time(NULL));
    for (i = 0; i < n; ++i) {
-      r = rand() % 10;
-      printf("- r: %d\n", r);
+      wait[i] = rand() % 10;
+      if (DEBUG) {
+         printf("- r: %d\n", wait[i]);
+      }
    }
 
    queue odd_pid, even_pid;
@@ -76,6 +92,65 @@ int main(int argc, char * argv[]) {
    initQueue(&even_pid);
 
    for (i = 0; i < n; i++) {
+      currPid = fork();
+      if (currPid == 0) {
+         // child process
+         break;
+      } else {
+         // parent
+
+         if (DEBUG) {
+            printf("--- child: i:%d:pid:%d:r:%d:\n", i, currPid, wait[i]);
+         }
+
+         kill(currPid, SIGSTOP);
+         pid[i] = currPid;
+         if (currPid % 2 == 1) {
+            enqueue(&odd_pid, i);
+         } else {
+            enqueue(&even_pid, i);
+         }
+      }
+   }
+
+   if (currPid == 0) {
+      // child process
+
+      sleep(wait[i]);
+      exit(0);
+   } else {
+      if (DEBUG) {
+         debugPrintQueue(odd_pid);
+         debugPrintQueue(even_pid);
+      }
+
+      while (odd_pid.front != nil || even_pid.front != nil) {
+         if (odd_pid.front != nil) {
+            dequeue(&odd_pid, &nextToResume);
+
+            totalOdd += wait[nextToResume];
+            printf("Child with pid %d is executing for %d seconds\n",
+                  pid[nextToResume], wait[nextToResume]);
+
+            kill(pid[nextToResume], SIGCONT);
+            waitpid(pid[nextToResume], NULL, 0);
+         }
+
+         if (even_pid.front != nil) {
+            dequeue(&even_pid, &nextToResume);
+
+            totalEven += wait[nextToResume];
+            printf("Child with pid %d is executing for %d seconds\n",
+                  pid[nextToResume], wait[nextToResume]);
+
+            kill(pid[nextToResume], SIGCONT);
+            waitpid(pid[nextToResume], NULL, 0);
+         }
+      }
+
+      printf("Total time assigned to odd queue:  %d\n", totalOdd);
+      printf("Total time assigned to even queue: %d\n", totalEven);
+      printf("Total time assigned for children:  %d\n", totalOdd + totalEven);
    }
 
    return 0;
